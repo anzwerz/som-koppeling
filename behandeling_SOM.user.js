fdd8a65c3d5408b03eac166fe94cfe1171beb47f
robert-anzwerz <robert@anzwerz.ai>
2025-07-16 16:33:44 +0200

    versie test

--- behandeling_SOM.user.js
+++ behandeling_SOM.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Behandeling SOM
 // @namespace    http://tampermonkey.net/
-// @version      1.0.5
+// @version      1.0.6
 // @description  Behandeling inladen voor SOM
 // @match        *://*/*
 // @grant        none
