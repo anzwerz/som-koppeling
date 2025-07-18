a17ddf4c12164b3c8772a12439601dfd943b95b1
robert-anzwerz <robert@anzwerz.ai>
2025-07-18 12:51:34 +0200

    versie 1.0.2

--- tm_test.user.js
+++ tm_test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Test TM
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Test
 // @match        *://*/*
 // @grant        none
